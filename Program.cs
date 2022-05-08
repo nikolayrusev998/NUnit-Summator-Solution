128f94caaa2fa6c55a7e858b124fe424a42b9719
Nikolay Rusev <105153477+nikolayrusev998@users.noreply.github.com>
2022-05-08 16:22:44 +0300

    Add project

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Summator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Summator.Sum(new int[] { 1, 2, 3, 4, 5, 6 });
+            if (result == 21)
+            {
+                Console.WriteLine("Test PASSED");
+            } else
+            {
+                Console.WriteLine("Test FAILED");
+            }
+        }
+    }
+}
